2f78ba1ea67b169a5e3d8da909e1e623aa6f8873
Mason John Hawver <hawve005@umn.edu>
2022-11-04 08:04:00 -0500

    working on api

--- backend/graphql.js
+++ backend/graphql.js
@@ -1,71 +1,56 @@
 const graphql = require("graphql");
 const db = require("./database.js");
 
-const IngredientType = new graphql.GraphQLObjectType({
-    name: "Ingredient",
-    fields: {
-        ingredient_id: { type: graphql.GraphQLID },
-        name: { type: graphql.GraphQLString }
+var schema = graphql.buildSchema(`
+    type Ingredient {
+        ingredient_id : ID
+        name: String
     }
-});
 
-var queryType = new graphql.GraphQLObjectType({
-    name: 'Query',
-    fields: {
-        ingredients: {
-            type: new graphql.GraphQLList(IngredientType),
-            resolve: (root, args, context, info) => {
-                return new Promise((resolve, reject) => {
-                    db.all("SELECT * FROM ingredients;", function (err, rows) {
-                        if (err) { reject([]); }
-                        resolve(rows);
-                    });
-                });
-
-            }
-        },
-        ingredient: {
-            type: IngredientType,
-            args: { id: { type: new graphql.GraphQLNonNull(graphql.GraphQLID) } },
-            resolve: (root, { id }, context, info) => {
-                return new Promise((resolve, reject) => {
-                  console.log(id);
-                    db.all("SELECT * FROM ingredients WHERE ingredient_id = (?);", [id], function (err, rows) {
-                        if (err) { reject(null); }
-                        resolve(rows[0]);
-                    });
-                });
-            }
-        }
+    type Recipe {
+        id : ID
+        name : String
+        ingredients : [Ingredient]
     }
-});
 
-var mutationType = new graphql.GraphQLObjectType({
-    name: 'Mutation',
-    fields: {
-      createIngredient: {
-        type: IngredientType,
-        args: {
-          name: { type: new graphql.GraphQLNonNull(graphql.GraphQLString) }
-        },
-        resolve: (root, {name}) => {
-          return new Promise((resolve, reject) => {
-              db.run('INSERT INTO ingredients (name) VALUES (?);', [name], (err) => {
-                  if(err) { reject(null); }
-                  db.get("SELECT last_insert_rowid() as id", (err, row) => {
-                      resolve({ id: row["id"], name: name });
-                  });
-              });
-          })
-        }
-      }
+    type Query {
+        all_ingredients: [Ingredient]
+        all_recipes: [Recipe]
+
+        ingredient(id: ID!) : Ingredient
+        recipe(id: ID!) : Recipe
+
+        message : String
     }
-});
+`);
 
+var root = {
+    message: () => 'Hello World!',
 
-const schema = new graphql.GraphQLSchema({
-    query: queryType,
-    mutation: mutationType
-});
+    all_ingredients: (root, args, context, info) => {
+        return new Promise((resolve, reject) => {
+            db.all("SELECT * FROM ingredients;", function (err, rows) {
+                if (err) { reject([]); }
+                console.log(rows);
+                resolve(rows);
+            }); 
+        });
+    },
 
-module.exports = { schema }
+    all_recipes: (root, args, context, info) => {
+        return new Promise((resolve, reject) => {
+            db.all("SELECT * FROM recipes;", function (err, rows) {
+                if (err) { reject([]); }
+                console.log(rows);
+                resolve(rows);
+            }); 
+        });
+    },
+
+    // Recipe: {
+    //     ingredients
+    // }
+
+};
+
+module.exports = { schema, root }
--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const schema = require("./graphql.js");
 
 const HTTP_PORT = 4000
 
-app.use("/graphql", ExpressGraphQL({ schema: schema.schema, graphiql: true}));
+app.use("/graphql", ExpressGraphQL({ schema: schema.schema, rootValue: schema.root, graphiql: true}));
 
 app.listen(HTTP_PORT, () => {
   console.log("Server running on http://localhost:%PORT%/".replace("%PORT%",HTTP_PORT));
