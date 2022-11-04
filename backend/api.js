a4b91ce3a72522c89801fa84f516aa4f6a68c597
Mason John Hawver <hawve005@umn.edu>
2022-11-04 09:59:38 -0500

    base api completed

---
+++ backend/api.js
@@ -0,0 +1,65 @@
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const { readFileSync } = require('fs')
+
+const SCHEMASOURCE = "schema.graphql"
+
+var typeDefs = readFileSync(SCHEMASOURCE).toString('utf-8')
+
+var resolvers = {
+  Query: {
+    message: () => 'Hello World!',
+
+    all_ingredients: (root, args, ctx, info) => {
+        return new Promise((resolve, reject) => {
+            ctx.db.all("SELECT ingredient_id as id, name FROM ingredients;", function (err, rows) {
+                if (err) { reject([]); }addResolversToSchema({ schema, resolvers })
+                resolve(rows);
+            });
+        });
+    },
+
+    ingredient: (root, {id}, ctx, info) => {
+      return new Promise((resolve, reject) => {
+          ctx.db.all("SELECT ingredient_id as id, name FROM ingredients where ingredients.ingredient_id = (?);", [id], function (err, rows) {
+              if (err) { reject([]); }
+              resolve(rows[0]);
+          });
+      });
+    },
+
+    all_recipes: (root, args, ctx, info) => {
+      return new Promise((resolve, reject) => {
+          ctx.db.all("SELECT recipe_id as id, name FROM recipes;", function (err, rows) {
+              if (err) { reject([]); }
+              resolve(rows);
+          });
+      });
+    },
+
+    recipe: (root, {id}, ctx, info) => {
+      return new Promise((resolve, reject) => {
+          ctx.db.all("SELECT recipe_id as id, name FROM recipes where recipes.recipe_id = (?);", [id], function (err, rows) {
+              if (err) { reject([]); }
+              resolve(rows[0]);
+          });
+      });
+    },
+
+  }, //end of querry
+
+  Recipe: {
+      ingredients : ({ id }, args, ctx, info) => {
+        return new Promise((resolve, reject) => {
+            ctx.db.all(`SELECT ing.ingredient_id as id, ing.name from quantities
+                        left join ingredients as ing on quantities.ingredient_id = ing.ingredient_id
+                        where quantities.recipe_id = (?);`, [id], function (err, rows) {
+                if (err) { reject([]); }
+                resolve(rows);
+            });
+        });
+      }
+  }
+
+};
+
+module.exports = { schema : makeExecutableSchema({ typeDefs, resolvers }) }
--- backend/database.js
+++ backend/database.js
@@ -4,7 +4,7 @@ const DBSOURCE = "sqlite/sqlite.db"
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
   if (err) { return console.error(err.message); }
-  console.log('Connected to the %DB% SQlite database.'.replace('%DB%', DBSOURCE));
+  console.log('Connected to the %DB% database.'.replace('%DB%', DBSOURCE));
 });
 
 module.exports = db
